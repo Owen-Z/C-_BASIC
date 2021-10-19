0e520ea9268d53a0289319707c2cf9be3fb2b448
Owen-Z <1696685042@qq.com>
2021-10-19 20:56:26 +0800

    First commit

---
+++ cmake-build-debug/CMakeFiles/C___Primer_Practice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___Primer_Practice.dir/main.cpp.obj"
+  "C___Primer_Practice.exe"
+  "C___Primer_Practice.exe.manifest"
+  "C___Primer_Practice.lib"
+  "C___Primer_Practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___Primer_Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
